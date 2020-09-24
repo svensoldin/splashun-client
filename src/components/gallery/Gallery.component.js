9d10368fefb455e695de8bc67ff6ae7dd4f22d78
Sven Soldin <soldinsven@gmail.com>
2020-09-24 15:57:16 +0200

    added react-masonry-css library, changed Gallery component to include the masonry and overlay on hover

--- src/components/gallery/Gallery.component.js
+++ src/components/gallery/Gallery.component.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { connect } from "react-redux";
+import axios from "axios";
+import Masonry from "react-masonry-css";
+
+//Selectors
 import { selectToken } from "../../redux/user/user.selectors";
 import { selectPictureAdded } from "../../redux/picture/picture.selectors";
-import axios from "axios";
 
 import "./Gallery.styles.css";
 
@@ -24,22 +27,28 @@ const Gallery = ({ token, search, picture }) => {
 		return picture.label.toLowerCase().includes(search.toLowerCase());
 	});
 
-	return pictures.length === 0 ? (
+	return !pictures.length ? (
 		<h2>LOADING</h2>
 	) : (
-		<Fragment>
-			<div className="gallery">
-				{filteredPictures.map((picture) => (
-					<li key={picture._id} className="list-element">
-						<img src={picture.imageURL} alt={picture.label}></img>
-						<div className="overlay">
-							<p className="label">{picture.label}</p>
-							<button className="delete">Delete</button>
-						</div>
-					</li>
-				))}
-			</div>
-		</Fragment>
+		<Masonry
+			breakpointCols={3}
+			className="masonry"
+			columnClassName="masonry-column"
+		>
+			{filteredPictures.map((picture) => (
+				<div key={picture._id} className="list-element">
+					<img
+						src={picture.imageURL}
+						alt={picture.label}
+						className="photo"
+					/>
+					<div className="overlay">
+						<p className="label">{picture.label}</p>
+						<button className="delete">X</button>
+					</div>
+				</div>
+			))}
+		</Masonry>
 	);
 };
 
@@ -51,3 +60,17 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(Gallery);
+
+// // <Fragment>
+// <div className="gallery">
+// 	{filteredPictures.map((picture) => (
+// 		<li key={picture._id} className="list-element">
+// 			<img src={picture.imageURL} alt={picture.label}></img>
+// 			<div className="overlay">
+// 				<p className="label">{picture.label}</p>
+// 				<button className="delete">Delete</button>
+// 			</div>
+// 		</li>
+// 	))}
+// </div>;
+// // </Fragment>
