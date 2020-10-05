78439d21c563f514fee80a84ac9c5e749a9a9009
Sven Soldin <soldinsven@gmail.com>
2020-10-05 11:14:11 +0200

    Updates

---
+++ precache-manifest.cc79e472443d95971145e111ed7f10c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc508545ca52d727e7489e1ce002ebc6",
+    "url": "/splashun-client/index.html"
+  },
+  {
+    "revision": "07ad7887617970fb92c8",
+    "url": "/splashun-client/static/css/2.89e89512.chunk.css"
+  },
+  {
+    "revision": "27ea9a57d3320b48a364",
+    "url": "/splashun-client/static/css/main.1604a374.chunk.css"
+  },
+  {
+    "revision": "07ad7887617970fb92c8",
+    "url": "/splashun-client/static/js/2.3b22ee51.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/splashun-client/static/js/2.3b22ee51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ea9a57d3320b48a364",
+    "url": "/splashun-client/static/js/main.03b38ae9.chunk.js"
+  },
+  {
+    "revision": "72ef005e823d344ee770",
+    "url": "/splashun-client/static/js/runtime-main.274d7c8a.js"
+  },
+  {
+    "revision": "b3947a026c6580e78794aaa5405919b0",
+    "url": "/splashun-client/static/media/camera.b3947a02.svg"
+  }
+]);
